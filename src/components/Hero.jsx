7c11abfd5dc0a9e6607474e4e1ef0a1e33036644
CrystalW11 <crystal.warmack@gmail.com>
2024-10-28 22:57:40 -0600

    Updated Hero.jsx and fixed styling issues

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { ComputersCanvas } from './canvas';
 const Hero = () => {
   return (
     <>
-      <section className="relative w-full h-[calc(100vh-120px)] sm:h-screen mx-auto">
+      <section className="relative w-full h-screen mx-auto">
         <div
           className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
           <div className="flex flex-col justify-center items-center mt-5">
@@ -23,7 +23,7 @@ const Hero = () => {
             </p>
           </div>
         </div>
-        <div className="sm:block hidden">
+        <div>
           <ComputersCanvas />
         </div>
         <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
