77266f1657bf67d31c514e4290ab9604c925defa
CrystalW11 <crystal.warmack@gmail.com>
2024-10-29 10:24:06 -0600

    updated scrolling

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // Step 4: Increase the chunk size limit
-  build: {
-    chunkSizeWarningLimit: 1000, // Adjust this limit as needed (in KB)
-  },
-
-  // Step 5: Optimize dependencies for pre-bundling
-  optimizeDeps: {
-    include: ["large-dependency"], // Add specific libraries you want to pre-bundle
-  },
 });
