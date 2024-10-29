e6094e3b47dd4012e96bcbcd5b238c7cc6f099c1
CrystalW11 <crystal.warmack@gmail.com>
2024-10-29 10:06:53 -0600

    restored the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,15 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import legacy from "@vitejs/plugin-legacy"; // Step 1: Import legacy plugin
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ["defaults", "not IE 11", "chrome >= 60", "android >= 5"], // Step 2: Set compatibility
-    }),
-  ],
+  plugins: [react()],
 
   // Step 4: Increase the chunk size limit
   build: {
-    target: "es2015", // Step 3: Set build target to support older syntax
     chunkSizeWarningLimit: 1000, // Adjust this limit as needed (in KB)
-    sourcemap: true, // Optional: Enable source maps for debugging
   },
 
   // Step 5: Optimize dependencies for pre-bundling
