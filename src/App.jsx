8ba51eb444a4b4186758f098c497373a5b0a36bd
CrystalW11 <crystal.warmack@gmail.com>
2024-10-29 09:39:45 -0600

    update loader.jsx and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
 
-import {About,Contact,Experience,Feedbacks,Hero,Navbar,Tech,Works,StarsCanvas} from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import React, { Suspense } from "react";
+import ComputersCanvas from "./canvas/Computers.jsx";
+import CanvasLoader from "./canvas/Loader.jsx"; // Correct path for the loader
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
         <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
+            <Suspense fallback={<CanvasLoader />}>
+              <ComputersCanvas />
+            </Suspense>
             <Hero />
           </div>
           <About />
