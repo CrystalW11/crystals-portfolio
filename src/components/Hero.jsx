81e9b31881a2d4d40e3404163fbddcd9b0d9a41f
CrystalW11 <crystal.warmack@gmail.com>
2024-10-28 22:54:01 -0600

    Updated Hero.jsx and fixed styling issues

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { ComputersCanvas } from './canvas';
 const Hero = () => {
   return (
     <>
-      <section className="relative w-full h-screen mx-auto">
+      <section className="relative w-full h-[calc(100vh-120px)] sm:h-screen mx-auto">
         <div
           className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
           <div className="flex flex-col justify-center items-center mt-5">
@@ -18,25 +18,28 @@ const Hero = () => {
               Hi, I'm <span className="text-[#915eff]">Crystal Warmack</span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop 3D visuals, user <br className='sm:block hidden'/> interfaces and web applications.
+              I develop 3D visuals, user <br className="sm:block hidden" />{" "}
+              interfaces and web applications.
             </p>
           </div>
         </div>
-        <ComputersCanvas />
-        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <div className="sm:block hidden">
+          <ComputersCanvas />
+        </div>
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
           <a href="#about">
             <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-              {/* <motion.div 
+              <motion.div
                 animate={{
-                  y: [ 0, 24, 0 ],
+                  y: [0, 24, 0],
                 }}
                 transition={{
                   duration: 1.5,
                   repeat: Infinity,
-                  repeatType:"loop",
+                  repeatType: "loop",
                 }}
                 className="w-3 h-3 rounded-full bg-secondary mb-1"
-              /> */}
+              />
             </div>
           </a>
         </div>
